5c0d42254ae647a81e6546bdd95ec140303691b5
Olya <Olya@DESKTOP-O0OQ4HF>
2019-11-12 00:13:26 +0300

    -added CheckStickersTest -divided Tests and TestBase

--- TestProject/MyFirstTest.cs
+++ TestProject/MyFirstTest.cs
@@ -1,25 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using Assert = NUnit.Framework.Assert;
 
-namespace TestProject
+namespace TestProject 
 {
     [TestFixture]
-    public class MyFirstTest
+    public class MyFirstTest : TestBase
     {
-        private ChromeDriver driver;
-        private WebDriverWait wait;
-
-        [SetUp]
-        public void Start()
-        {
-            driver = new ChromeDriver();
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            driver.Manage().Window.Maximize();
-        }
 
         /*[Test]
         public void FirstTest()
@@ -28,7 +20,7 @@ namespace TestProject
         }*/
 
         [Test]
-        public void LoginTest() 
+        public void LoginTest()
         {
             driver.Url = "http://localhost/litecart/admin/login.php";
             driver.FindElement(By.Name("username")).SendKeys("admin");
@@ -36,11 +28,51 @@ namespace TestProject
             driver.FindElement(By.Name("login")).Click();
         }
 
-        [TearDown]
-        public void Stop() 
+        //[Test]
+        //public void CheckMenuTest() 
+        //{
+        //    driver.Url = "http://localhost/litecart/admin";
+        //    Login();
+        //    IWebElement panel = driver.FindElement(By.Id("box-apps-menu"));
+        //    IList<IWebElement> items = panel.FindElements(By.TagName("li"));
+        //    for (int i = 1; i <= items.Count; i++) 
+        //    {
+        //        panel = driver.FindElement(By.Id("box-apps-menu"));
+        //        IWebElement item = panel.FindElement(By.XPath("//li[" + i + "]"));
+        //        try
+        //        {
+        //            item.Click();
+        //            panel = driver.FindElement(By.Id("box-apps-menu"));
+        //            item = panel.FindElement(By.XPath("//li[" + i + "]"));
+        //            IList<IWebElement> subItems = item.FindElements(By.TagName("li"));
+                    
+        //            System.Threading.Thread.Sleep(2000);
+        //        }
+        //        catch (StaleElementReferenceException) 
+        //        {
+        //            item = panel.FindElement(By.XPath("//li[" + i + "]"));
+        //            item.Click();
+        //            System.Threading.Thread.Sleep(2000);
+        //        }
+
+        //    }
+           
+        //}
+
+        [Test]
+        public void CheckStickersTest() 
         {
-            driver.Quit();
-            driver = null;
+            driver.Url = "http://localhost/litecart";
+            IList<IWebElement> products = driver.FindElements(By.ClassName("image-wrapper"));
+            foreach (IWebElement product in products) 
+            {
+                IList<IWebElement> stickersNew = product.FindElements(By.XPath("./div[@class='sticker new']"));
+                IList<IWebElement> stickersSale = product.FindElements(By.XPath("./div[@class='sticker sale']"));
+                int stickersTotal = stickersNew.Count + stickersSale.Count;
+                Assert.IsTrue(stickersTotal == 1, "stickers count was " + stickersTotal);
+            }
+
         }
+       
     }
 }
---
+++ TestProject/TestBase.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace TestProject
+{
+    public class TestBase
+    {
+        public IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+        }
+
+        public bool IsElementPresent(IWebDriver driver, By locator)
+        {
+            try
+            {
+                wait.Until(ExpectedConditions.ElementExists(locator));
+                //driver.FindElement(locator);
+                return true;
+            }
+            catch (NoSuchElementException ex)
+            {
+                return false;
+            }
+        }
+        public bool AreElementsPresent(IWebDriver driver, By locator)
+        {
+            return driver.FindElements(locator).Count > 0;
+        }
+
+        public void Login() 
+        {
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+        }
+        [TearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
