b8a42dc355f27635acc20503154fecd0e169d756
Olya <Olya@DESKTOP-O0OQ4HF>
2019-11-11 14:24:46 +0300

    Added my first simple test

---
+++ TestProject/MyFirstTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace TestProject
+{
+    [TestFixture]
+    public class MyFirstTest
+    {
+        private ChromeDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://www.google.com/";
+        }
+
+        [TearDown]
+        public void Stop() 
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
