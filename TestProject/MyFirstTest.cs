82fb9043fb831d7c5d3cce018d5784203071af33
Olya <Olya@DESKTOP-O0OQ4HF>
2019-11-11 15:21:14 +0300

    added LoginTest

--- TestProject/MyFirstTest.cs
+++ TestProject/MyFirstTest.cs
@@ -21,10 +21,19 @@ namespace TestProject
             driver.Manage().Window.Maximize();
         }
 
-        [Test]
+        /*[Test]
         public void FirstTest()
         {
             driver.Url = "http://www.google.com/";
+        }*/
+
+        [Test]
+        public void LoginTest() 
+        {
+            driver.Url = "http://localhost/litecart/admin/login.php";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
         }
 
         [TearDown]
